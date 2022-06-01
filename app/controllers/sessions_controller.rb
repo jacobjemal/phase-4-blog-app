655c5fd1fa3711c9a47d2c066ee788685d8c6f42
jacob jemal <jacobjemal7@gmail.com>
2022-06-01 11:41:15 -0400

    stated bakend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+
+
+    def login
+        user = User.find_by(name:params[:username])
+        
+        if user&.authenticate(params[:password])
+
+            session[:current_user] = user.id
+
+            session[:login_attempts] = 0
+            
+            render json: user, status: :ok
+        else
+
+            session[:login_attempts] ||= 0
+
+            session[:login_attempts] += 1
+
+            render json: { error: "Invalid Password and/or Username" },  status: :unauthorized
+        end
+    end 
+
+    def logout
+        session.delete :current_user
+    end 
 end
