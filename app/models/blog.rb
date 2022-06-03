d7f3f4ca9334552a2aafdc676f58705ddde17a5f
jacob jemal <jacobjemal7@gmail.com>
2022-06-03 13:04:24 -0400

    backend added dependent destroy

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
-    belongs_to :user
-    has_many :comments
+    belongs_to :user, dependent: :destroy
+    has_many :comments, dependent: :destroy
 
     validates :title, presence: true, uniqueness: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :blog
+    belongs_to :user, dependent: :destroy
+    belongs_to :blog, dependent: :destroy
 end
